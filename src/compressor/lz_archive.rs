700082630e9f7b2fc4fec431d0ffca6c0911f67d
sevonj <100710152+sevonj@users.noreply.github.com>
2025-09-25 13:04:08 +0300

    refactor: lz pointer own module

--- src/compressor/lz_archive.rs
+++ src/compressor/lz_archive.rs
@@ -1,6 +1,8 @@
 use std::io::Read;
 use std::io::Write;
 
+use super::LzPointer;
+
 pub struct LzArchive {
     data: Vec<u8>, // Uncompressed
 }
@@ -29,102 +31,8 @@ impl LzArchive {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct LzPointer {
-    off: isize, // Relative to current read pos
-    len: usize, // Number of bytes
-}
-
-impl LzPointer {
-    pub const fn off(&self) -> isize {
-        self.off
-    }
-
-    pub const fn len(&self) -> usize {
-        self.len
-    }
-
-    pub fn new(off: isize, len: usize) -> Self {
-        Self { off, len }
-    }
-
-    pub fn find(input: &[u8], input_pos: usize) -> Result<Option<Self>, std::io::Error> {
-        let mut best_len = 0;
-        let mut best_off = 0;
-
-        for off in 0..input_pos {
-            for (i, byte) in input[off..].iter().enumerate() {
-                let off_input_char = input_pos + i;
-                if i > best_len {
-                    best_off = off;
-                    best_len = i;
-                }
-                if off_input_char == input.len() {
-                    break;
-                }
-                if *byte != input[off_input_char] {
-                    break;
-                }
-                println!("match: {i}, len: {best_len}, off: {best_off}");
-            }
-        }
-
-        if best_len < 3 {
-            return Ok(None);
-        }
-
-        Ok(Some(Self {
-            off: best_off as isize - input_pos as isize,
-            len: best_len,
-        }))
-    }
-}
-
 #[cfg(test)]
 mod tests {
 
     use super::*;
-
-    #[test]
-    fn test_pointer_find_match() {
-        let input = b"Rep_eat Repeat Repeat";
-
-        // pos 8 is the second 'R'
-        let result = LzPointer::find(input, 8).unwrap().unwrap();
-
-        assert_eq!(result, LzPointer::new(-8, 3));
-    }
-
-    // Match ends after input pos
-    #[test]
-    fn test_pointer_find_match_exceed() {
-        let input = b"abcd_abcd_abcd-xyzxyzxyzx";
-
-        // pos 7 is the second 'c'
-        let result = LzPointer::find(input, 7).unwrap().unwrap();
-
-        assert_eq!(result, LzPointer::new(-5, 7));
-    }
-
-    // Tests match minimum limit. TODO: actually think about the limit
-    #[test]
-    fn test_pointer_find_match_3b() {
-        let input = b"abcdefffghifffjklmn";
-
-        // pos 11 is the start of the second "fff"
-        let result = LzPointer::find(input, 11).unwrap().unwrap();
-
-        assert_eq!(result, LzPointer::new(-6, 3));
-    }
-
-    // Tests match minimum limit. TODO: actually think about the limit
-    #[test]
-    fn test_pointer_find_match_2b() {
-        let input = b"abcdeffxghiffyjklmn";
-
-        // pos 11 is the start of the second "ff"
-        let result = LzPointer::find(input, 11).unwrap();
-
-        assert_eq!(result, None);
-    }
 }
--- src/compressor/mod.rs
+++ src/compressor/mod.rs
@@ -3,6 +3,7 @@ mod huffman_code;
 mod huffman_io;
 mod huffman_tree;
 mod lz_archive;
+mod lz_pointer;
 
 pub use huffman_archive::HuffmanArchive;
 use huffman_code::HuffmanCode;
@@ -10,3 +11,4 @@ use huffman_io::CodeReader;
 use huffman_io::CodeWriter;
 use huffman_tree::HuffmanTreeNode;
 pub use lz_archive::LzArchive;
+use lz_pointer::LzPointer;
