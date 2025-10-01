a85c4a3908581a28cc3ba735c1d442a667cee3f7
sevonj <100710152+sevonj@users.noreply.github.com>
2025-10-01 13:26:34 +0300

    refactor: rename lz io to bit io

--- src/compressor/mod.rs
+++ src/compressor/mod.rs
@@ -1,17 +1,17 @@
+mod bit_io;
 mod huffman_archive;
 mod huffman_code;
 mod huffman_io;
 mod huffman_tree;
 mod lz_archive;
-mod lz_io;
 mod lz_pointer;
 
+use bit_io::BitReader;
+use bit_io::BitWriter;
 pub use huffman_archive::HuffmanArchive;
 use huffman_code::HuffmanCode;
 use huffman_io::CodeReader;
 use huffman_io::CodeWriter;
 use huffman_tree::HuffmanTreeNode;
 pub use lz_archive::LzArchive;
-use lz_io::BitReader;
-use lz_io::BitWriter;
 use lz_pointer::LzPointer;
