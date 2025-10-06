6aff2052b5fcd3533d0e1e616463ea4b2efc6ff1
sevonj <100710152+sevonj@users.noreply.github.com>
2025-10-06 15:38:47 +0300

    refactor: cleanup LZ ptr

--- src/compressor/lz_pointer.rs
+++ src/compressor/lz_pointer.rs
@@ -33,14 +33,14 @@ impl LzPointer {
         self.len
     }
 
-    pub const fn new(off: usize, len: usize) -> Self {
-        debug_assert!(off <= Self::MAX_OFF);
+    pub const fn new(len: usize, off: usize) -> Self {
         debug_assert!(len <= Self::MAX_LEN);
+        debug_assert!(off <= Self::MAX_OFF);
 
-        Self { off, len }
+        Self { len, off }
     }
 
-    /// Not a pointer, literal data
+    /// Zero pointer for literal byte
     pub const fn literal() -> Self {
         Self { off: 0, len: 0 }
     }
@@ -50,7 +50,7 @@ impl LzPointer {
         self.len == 0
     }
 
-    /// Deserialize.
+    /// Deserialize, bit-packed
     pub fn read<R: Read>(bitreader: &mut BitReader<R>) -> Result<Self, std::io::Error> {
         let mut len = 0;
         for _ in 0..Self::BITS_IN_LEN {
@@ -58,7 +58,7 @@ impl LzPointer {
             len += bitreader.read_bit()? as usize;
         }
 
-        // Not a pointer, we can skip offset bytes
+        // Zero ptr, we can skip offset bytes
         if len == 0 {
             return Ok(Self::literal());
         }
@@ -69,16 +69,16 @@ impl LzPointer {
             off += bitreader.read_bit()? as usize;
         }
 
-        Ok(Self { len, off })
+        Ok(Self::new(len, off))
     }
 
-    /// Serialize
+    /// Serialize, bit-packed
     pub fn write<W: Write>(&self, bitwriter: &mut BitWriter<W>) -> Result<(), std::io::Error> {
         for i in (0..Self::BITS_IN_LEN).rev() {
             bitwriter.write(1, (self.len >> i) as u8)?;
         }
 
-        // Not a pointer, we can skip offset bytes
+        // Zero ptr, we can skip offset bytes
         if self.len == 0 {
             return Ok(());
         }
@@ -90,14 +90,14 @@ impl LzPointer {
         Ok(())
     }
 
-    /// Find longest match
+    /// Find longest match that starts before `input_pos`
     pub fn find(input: &[u8], input_pos: usize) -> Result<Self, std::io::Error> {
-        let input_start = input_pos.saturating_sub(Self::MAX_OFF);
+        let search_start = input_pos.saturating_sub(Self::MAX_OFF);
 
         let mut best_len = 0;
         let mut best_off = 0;
 
-        for off in input_start..input_pos {
+        for off in search_start..input_pos {
             // When either max len or end of input buffer is reached.
             let too_far = std::cmp::min(input_pos + Self::MAX_LEN, input.len());
 
@@ -116,7 +116,8 @@ impl LzPointer {
             }
         }
 
-        if best_len < 3 {
+        // Pointing back to 1 byte long matches actually wastes space.
+        if best_len < 2 {
             return Ok(Self::literal());
         }
 
@@ -129,28 +130,6 @@ impl LzPointer {
         })
     }
 }
-
-/*
-impl From<u16> for LzPointer {
-    fn from(value: u16) -> Self {
-        let off = value & 0xfff;
-        let len = value >> 12;
-
-        Self {
-            off: off as usize,
-            len: len as usize,
-        }
-    }
-}
-
-impl From<LzPointer> for u16 {
-    fn from(value: LzPointer) -> Self {
-        let off = value.off as u16;
-        let len = (value.len << 12) as u16;
-        off + len
-    }
-}
-// */
 
 #[cfg(test)]
 mod tests {
@@ -166,7 +145,7 @@ mod tests {
         // pos 8 is the second 'R'
         let result = LzPointer::find(input, 8).unwrap();
 
-        assert_eq!(result, LzPointer::new(8, 3));
+        assert_eq!(result, LzPointer::new(3, 8));
     }
 
     // Match ends after input pos
@@ -177,10 +156,10 @@ mod tests {
         // pos 7 is the second 'c'
         let result = LzPointer::find(input, 7).unwrap();
 
-        assert_eq!(result, LzPointer::new(5, 7));
+        assert_eq!(result, LzPointer::new(7, 5));
     }
 
-    // Tests match minimum limit. TODO: actually think about the limit
+    // Tests match minimum limit.
     #[test]
     fn test_pointer_find_match_3b() {
         let input = b"abcdefffghifffjklmn";
@@ -188,10 +167,10 @@ mod tests {
         // pos 11 is the start of the second "fff"
         let result = LzPointer::find(input, 11).unwrap();
 
-        assert_eq!(result, LzPointer::new(6, 3));
+        assert_eq!(result, LzPointer::new(3, 6));
     }
 
-    // Tests match minimum limit. TODO: actually think about the limit
+    // Tests match minimum limit.
     #[test]
     fn test_pointer_find_match_2b() {
         let input = b"abcdeffxghiffyjklmn";
@@ -199,24 +178,19 @@ mod tests {
         // pos 11 is the start of the second "ff"
         let result = LzPointer::find(input, 11).unwrap();
 
+        assert_eq!(result, LzPointer::new(2, 6));
+    }
+
+    // Tests match minimum limit.
+    #[test]
+    fn test_pointer_find_match_1b() {
+        let input = b"abcdeffxghifyjklmn";
+
+        // pos 11 is the start of the second "ff"
+        let result = LzPointer::find(input, 11).unwrap();
+
         assert!(result.is_literal());
     }
-
-    /*
-    #[test]
-    fn test_pointer_from_u16() {
-        assert_eq!(LzPointer::from(0x1234), LzPointer::new(0x234, 0x1));
-        assert_eq!(LzPointer::from(0xffff), LzPointer::new(0xfff, 0xf));
-        assert_eq!(LzPointer::from(0x0ff0), LzPointer::new(0xff0, 0x0));
-    }
-
-    #[test]
-    fn test_pointer_to_u16() {
-        assert_eq!(u16::from(LzPointer::new(0x512, 0x3)), 0x3512);
-        assert_eq!(u16::from(LzPointer::new(0x00f, 0xe)), 0xe00f);
-        assert_eq!(u16::from(LzPointer::new(0xff0, 0x0)), 0x0ff0);
-    }
-    // */
 
     #[test]
     fn test_pointer_read_none() {
@@ -236,7 +210,7 @@ mod tests {
 
         assert_eq!(
             LzPointer::read(&mut bitreader).unwrap(),
-            LzPointer::new(0b_111100001111, 0b_0100)
+            LzPointer::new(0b_0100, 0b_111100001111)
         );
     }
 
@@ -258,13 +232,13 @@ mod tests {
         let mut data = vec![];
         let mut bitwriter = BitWriter::new(&mut data);
 
-        let ptr_a = LzPointer::new(0b_0101_0001_1000, 0b_0101);
-        let ptr_b = LzPointer::new(0b_0001_0000_0110, 0b_0001);
-        let ptr_c = LzPointer::new(0b_0000_0000_0000, 0b_1101);
-        let ptr_d = LzPointer::new(0b_1111_1111_1110, 0b_1110);
-        let ptr_e = LzPointer::new(0b_1111_0110_0000, 0b_1111);
-        let ptr_f = LzPointer::new(0b_0110_0011_0000, 0b_0110);
-        let ptr_g = LzPointer::new(0b_0111_0000_0111, 0b_0111);
+        let ptr_a = LzPointer::new(0b_0101, 0b_0101_0001_1000);
+        let ptr_b = LzPointer::new(0b_0001, 0b_0001_0000_0110);
+        let ptr_c = LzPointer::new(0b_1101, 0b_0000_0000_0000);
+        let ptr_d = LzPointer::new(0b_1110, 0b_1111_1111_1110);
+        let ptr_e = LzPointer::new(0b_1111, 0b_1111_0110_0000);
+        let ptr_f = LzPointer::new(0b_0110, 0b_0110_0011_0000);
+        let ptr_g = LzPointer::new(0b_0111, 0b_0111_0000_0111);
 
         ptr_a.write(&mut bitwriter).unwrap();
         ptr_b.write(&mut bitwriter).unwrap();
